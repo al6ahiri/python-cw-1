8b533b81463fa672c0961cb4427da854236c8a8d
al6ahiri <109921882+al6ahiri@users.noreply.github.com>
2023-08-23 19:54:41 +0300

    done

---
+++ sa.py
@@ -0,0 +1,17 @@
+name = ("saud")
+
+print (4+4)
+print (2/4)
+print (2*2)
+
+length = 10
+width = 10 
+area = length * width 
+
+print(f"the ruselt is {area}")
+
+length = 5
+width = 3
+area = 15
+
+print (f"if the length is {length} , and width is {width} , then the area is {area}")
